bd7ea9cc1eca50a269b7ce17565fe4d9077e4f76
egordmitriev-creator <edmitriev377@gmail.com>
2025-04-19 14:22:56 +0700

    Jenkins for pipeline

---
+++ lab_7-8/Jenkins/test_redfish.py
@@ -0,0 +1,69 @@
+import pytest
+import requests
+import logging
+
+# Настройка логирования 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler("test_redfish.log", mode="w")
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+logger.addHandler(file_handler)
+
+BASE_URL = "https://localhost:2443/redfish/v1"
+
+USERNAME = "root"
+PASSWORD = "0penBmc"
+
+# Фикстура для созданике сессии
+@pytest.fixture(scope="module")
+def session():
+    session = requests.Session()
+    session.auth = (USERNAME, PASSWORD)
+    session.verify = False
+    yield session
+    session.close()
+
+# Тест аутентификации
+def test_authentication(session):
+    auth_url = f"{BASE_URL}/SessionService/Sessions"
+
+    auth_data ={
+        "UserName": f"{USERNAME}",
+        "Password": f"{PASSWORD}"
+    }
+
+    response = session.post(auth_url, json = auth_data)
+    assert response.status_code == 201, "Authentication error" # При выполнении post запрса возвращает 201 (Создано)
+    session_info = response.json()
+    assert "@odata.id" in session_info, "The Session token field is missing in the response"
+    logger.info("The authentication test was completed successfuly")
+
+# Тест получения информации о системе
+def test_get_system_info(session):
+    system_url = f"{BASE_URL}/Systems/system"
+    response = session.get(system_url)
+    assert response.status_code == 200, "Error when receiving information about the system"
+    system_info = response.json()
+    assert "Status" in system_info, "The Status field is missing in the response"
+    assert "PowerState" in system_info, "The PowerState field is missing in the response"
+    logger.info("The system information acquisition test was completed successfully")
+
+# Тест управления питанием 
+def test_pover_on(session):
+    pover_usl = f"{BASE_URL}/Systems/system/Actions/ComputerSystem.Reset"
+
+    pover_data = {
+        "ResetType": "On"
+    }
+
+    response = session.post(pover_usl, json = pover_data)
+    assert response.status_code == 204, "Error when trying to turn on the server" # Текущая версия Bmc возвращает 204 
+
+    # Проверка изменения статуса системы
+    system_url = f"{BASE_URL}/Systems/system"
+    response = session.get(system_url)
+    system_info = response.json()
+
+    assert system_info["PowerState"] == "Off", "The server did not turn on" # Off -  заглушка, текущая версия Bmc не включается
+    logger.info("The power management test was completed successfully")
---
+++ lab_7-8/Jenkins/test_webui.py
@@ -0,0 +1,101 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+# Фикстура для инициализации и завершения работы драйвера
+@pytest.fixture(scope="module")
+def driver():
+    # Настройка опций Chrome
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--headless") # Для CI
+
+    service = Service(executable_path="/usr/bin/chromedriver")
+
+    # Инициализация драйвера
+    driver = webdriver.Chrome(service=service, options=options)
+    yield driver  # Возвращаем драйвер тесту
+
+    # Завершение работы драйвера после выполнения теста
+    driver.quit()
+
+# Тест для проверки авторизации
+def test_success_login(driver):
+    # Переход на страницу авторизации
+    driver.get('https://localhost:2443/?next=/login#/login')
+    time.sleep(3)  # Ожидание загрузки страницы
+
+    # Поиск элементов на странице
+    username = driver.find_element(By.ID, 'username')
+    password = driver.find_element(By.ID, 'password')
+    login_button = driver.find_element(By.CLASS_NAME, 'btn.btn-primary.mt-3')
+
+    # Ввод данных для авторизации
+    username.send_keys('root')
+    password.send_keys('0penBmc')
+
+    # Нажатие кнопки входа
+    login_button.click()
+    time.sleep(3)  # Ожидание завершения авторизации
+
+    successWindow = driver.find_element(By.CLASS_NAME, 'app-container')
+
+    # Проверка успешной авторизации (пример проверки)
+    assert successWindow.is_displayed()
+
+def test_fail_login(driver):
+    # Переход на страницу авторизации
+    driver.get('https://localhost:2443/?next=/login#/login')
+    time.sleep(3)
+
+    username = driver.find_element(By.ID, 'username')
+    password = driver.find_element(By.ID, 'password')
+    login_button = driver.find_element(By.CLASS_NAME, 'btn.btn-primary.mt-3')
+
+    # Ввод данных для авторизации
+    username.send_keys('root')
+    password.send_keys('OpenBmc') # Не верный пароль
+
+    login_button.click()
+    time.sleep(3)
+
+    errorWindow = driver.find_element(By.CLASS_NAME, 'neterror')
+
+    assert errorWindow.is_displayed()
+
+def test_block_user(driver):
+    # Функция для попытки входа
+    def attempt_login(username_value, password_value):
+        driver.get('https://localhost:2443/?next=/login#/login')
+        time.sleep(3)
+        # Ожидание появления элементов формы
+        username = driver.find_element(By.ID, 'username')
+        password = driver.find_element(By.ID, 'password')
+        login_button = driver.find_element(By.CLASS_NAME, 'btn.btn-primary.mt-3')
+        
+        # Ввод данных для авторизации
+        username.send_keys(username_value)
+        password.send_keys(password_value)
+        login_button.click()
+
+    # Попытка входа с правильным паролем
+    attempt_login('user', 'somepass1')  # Правильный пароль
+    time.sleep(3)
+
+    # Три попытки входа с неверным паролем
+    for _ in range(3):
+        attempt_login('user', 'OpenBmc')  # Неверный пароль
+        time.sleep(3)
+
+    # Попытка входа с правильным паролем
+    attempt_login('user', 'somepass1')  # Правильный пароль
+    time.sleep(3)
+
+    errorWindow = driver.find_element(By.CLASS_NAME, 'neterror')
+
+    assert errorWindow.is_displayed()
